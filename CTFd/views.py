0c22ef1d8d70b94dbea8e4d888e627159f3d4217
ZenZenVibe <13644560+jossets@users.noreply.github.com>
2019-04-03 20:50:04 +0200

    Update views.py

--- CTFd/views.py
+++ CTFd/views.py
@@ -54,8 +54,8 @@ def setup():
 
             index = """<div class="row">
     <div class="col-md-6 offset-md-3">
-        <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="themes/core/static/img/logo.png" />
-        </br>
+        
+        
         
         <h4 class="text-center">
             <p>A cool CTF platform for Beginners</p></br></br>    
