3edef060e5791b672c2148272fdd0cad829a215b
ZenZenVibe <13644560+jossets@users.noreply.github.com>
2019-04-03 18:21:03 +0200

    Update views.py

--- CTFd/views.py
+++ CTFd/views.py
@@ -55,16 +55,12 @@ def setup():
             index = """<div class="row">
     <div class="col-md-6 offset-md-3">
         <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="themes/core/static/img/logo.png" />
-        <h3 class="text-center">
-            <p>A cool CTF platform from <a href="https://ctfd.io">ctfd.io</a></p>
-            <p>Follow us on social media:</p>
-            <a href="https://twitter.com/ctfdio"><i class="fab fa-twitter fa-2x" aria-hidden="true"></i></a>&nbsp;
-            <a href="https://facebook.com/ctfdio"><i class="fab fa-facebook fa-2x" aria-hidden="true"></i></a>&nbsp;
-            <a href="https://github.com/ctfd"><i class="fab fa-github fa-2x" aria-hidden="true"></i></a>
-        </h3>
         <br>
         <h4 class="text-center">
-            <a href="admin">Click here</a> to login and setup your CTF
+            <p>A cool CTF platform for Beginners</p>
+            <p>Follow us </p>
+            <a href="register">User click here</a> to login and start 
+            <a href="admin">Admin click here</a> to login and setup your CTF
         </h4>
     </div>
 </div>""".format(request.script_root)
